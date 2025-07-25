c22e216007b781591fb5e7a6050d0c036431c888
Nakesh_Tewari <nakesh1107@gmail.com>
2025-07-25 14:27:34 +0530

    changes

--- src/main/java/com/example/email_assistant/app/EmailGeneratorService.java
+++ src/main/java/com/example/email_assistant/app/EmailGeneratorService.java
@@ -93,7 +93,7 @@ public class EmailGeneratorService {
 
     public String buildPrompt(EmailRequest emailRequest){
         StringBuilder prompt= new StringBuilder();
-        prompt.append("Generate a professional email reply for the following email content.Please don't generate a subject line. ");
+        prompt.append("Generate a professional email in detail and reply for the following email content.Please don't generate a subject line. ");
 
         if(emailRequest.getTone()!=null && !emailRequest.getTone().isEmpty()){
             prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
